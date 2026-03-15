c0121a24a5d250f12e4661ce1c5e1157b21270c7
tharun01111 <tharunadhi33@gmail.com>
2026-03-15 15:20:30 +0530

    refactor: improve token generation and database connection error handling

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const {MONGO_URI} = process.env.MONGO_URI
+    if(!MONGO_URI) throw new Error("MONGO_URI not set");
+    const conn = await mongoose.connect(MONGO_URI);
     console.log("MongoDb connected on: " + conn.connection.host);
   } catch (error) {
     console.error("Error connecting to mongoDb: " + error);
