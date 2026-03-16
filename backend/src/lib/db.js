41132c69359ea72c28372bdb4a78cb7c84cc0b4e
tharun01111 <tharunadhi33@gmail.com>
2026-03-16 19:25:51 +0530

    welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDb = async () => {
+  console.log(ENV.MONGO_URI);
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    
     console.log("MongoDb connected on: " + conn.connection.host);
   } catch (error) {
     console.error("Error connecting to mongoDb: " + error);
