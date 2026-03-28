fdf90006abe54a2e71769eaf882af489f4f1aea3
tharun01111 <tharunadhi33@gmail.com>
2026-03-28 12:09:08 +0530

    singup implemented on frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        border: "border 4s linear infinite",
+      },
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
+      },
+    },
   },
   plugins: [daisyui],
 };
