6bc7a73b35445c1b44c872fe11f1cfd1d625c8fb
tharun01111 <tharunadhi33@gmail.com>
2026-04-03 21:47:26 +0530

    online users implemented

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
-import { useChatStore } from "../store/useChatStore";
 import { XIcon } from "lucide-react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatHeader() {
-  const { setSelectedUser, selectedUser } = useChatStore();
+  const { selectedUser, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+  const isOnline = onlineUsers.includes(selectedUser._id);
 
   useEffect(() => {
     const handleEscKey = (event) => {
@@ -12,28 +15,32 @@ function ChatHeader() {
 
     window.addEventListener("keydown", handleEscKey);
 
-    //cleanup function
-
+    // cleanup function
     return () => window.removeEventListener("keydown", handleEscKey);
   }, [setSelectedUser]);
 
   return (
-    <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
+    <div
+      className="flex justify-between items-center bg-slate-800/50 border-b
+   border-slate-700/50 max-h-[84px] px-6 flex-1"
+    >
       <div className="flex items-center space-x-3">
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : "offline"}`}>
           <div className="w-12 rounded-full">
             <img
               src={selectedUser.profilePic || "/avatar.png"}
               alt={selectedUser.fullName}
-              className=""
             />
           </div>
         </div>
+
         <div>
           <h3 className="text-slate-200 font-medium">
             {selectedUser.fullName}
           </h3>
-          <p className="text-slate-400 text-sm">Online</p>
+          <p className="text-slate-400 text-sm">
+            {isOnline ? "Online" : "Offline"}
+          </p>
         </div>
       </div>
 
@@ -43,5 +50,4 @@ function ChatHeader() {
     </div>
   );
 }
-
 export default ChatHeader;
